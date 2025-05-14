ff79a8e28941b63af9e6bb1029fa92bd0092440f
Zyrandool <zyrafka22@gmail.com>
2025-05-14 13:35:39 +0200

    Added translation to Primitives

--- assets/shaders/vertex_shader.glsl
+++ assets/shaders/vertex_shader.glsl
@@ -1,16 +1,23 @@
 #version 330 core
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 normal;
-layout(location = 2) in vec2 texCoords;
-layout(location = 3) in vec4 color;
 
-out vec4 vertexColor;
+// Atrybuty wejściowe wierzchołka
+layout (location = 0) in vec3 position;   // Pozycja wierzchołka
+layout (location = 1) in vec3 normal;     // Normalny wektor
+layout (location = 2) in vec2 texCoords;  // Współrzędne tekstury
+layout (location = 3) in vec4 color;      // Kolor wierzchołka
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+// Wyjścia do shadera fragmentów
+out vec4 vertexColor; // Bezpośrednie przekazanie koloru
+
+// Uniformy (macierze transformacji)
+uniform mat4 model;       // Macierz modelu
+uniform mat4 view;        // Macierz widoku
+uniform mat4 projection;  // Macierz projekcji
 
 void main() {
+    // Transformacja pozycji do przestrzeni przycinania
     gl_Position = projection * view * model * vec4(position, 1.0);
+
+    // Przekazanie koloru do shadera fragmentów
     vertexColor = color;
 }
